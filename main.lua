c8db28bc2a2a0367cf8f3950aa6eb83b8bb23fb7
m23660314-stack <m23660314@gmail.com>
2026-09-05 01:45:04 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,42 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+
+
+local Window = Fluent:CreateWindow({
+	Title = "MHKZ Hub " .. Fluent.Version,
+	SubTitle = "by MHKZ",
+	TabWidth = 160,
+	Size = UDim2.fromOffset(580, 460),
+	Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+	Theme = "Dark",
+	MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+Fluent:Notify({
+	Title = "Notification",
+	Content = "You Execute a script !", 
+	SubContent = "by MHKZ", -- Optional
+	Duration = 3 -- Set to nil to make the notification not disappear
+})
+
+-- Fluent provides Lucide Icons, they are optional
+local maintab = {
+	Main = Window:AddTab({ Title = "Player", Icon = nil }),
+	
+}
+
+local Section = maintab:AddSection("Player Settings")
+
+
+local Slider = maintab:AddSlider("Slider", 
+	{
+		Title = "Walkspeed ",
+		Description = "Change your walkspeed",
+		Default = 16,
+		Min = 0,
+		Max = 500,
+		Rounding = 1,
+		Callback = function(Value)
+			game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+		end
+	})
